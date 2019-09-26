7faf80d6d9c81b73cd68ead3956e7f435a90ce2f
Abhijith Mundanad <mabhijithn@gmail.com>
2019-09-26 10:31:10 +0200

    Intialized aadbasic as a package. Created setup file to enable adding package via pip.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='aadbasic',
+      version='0.1',
+      description='AAD using EEG:Python implementation',
+      author='Abhijith Mundanad Narayanan',
+      author_email='abhijith@esat.kuleuven.be',
+      packages=['aadbasic'])
